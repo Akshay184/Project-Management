cc8e62713e2c3525bc006f8e55398e8d964f632a
amanmishra17 <42470428+amanmishra17@users.noreply.github.com>
2019-01-28 19:16:18 +0530

    signup page created

---
+++ Documents/Visual Studio 2015/Projects/Project-Management/Project-Management/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_Management.Models;
+using System.Data.Entity;
+using System.Data;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using Microsoft.AspNet.Identity;
+using System.IO;
+
+namespace Project_Management.Controllers
+{ //[System.Runtime.InteropServices.Guid("0C2C6A46-45B7-4907-91DA-D86AC48944BB")]
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        
+        
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserRegistration NewUser)
+        {
+            
+            UserRegistration NewUser1 = new UserRegistration();
+            string Filename = Path.GetFileNameWithoutExtension(NewUser.ImageUpload.FileName);
+            string extension = Path.GetExtension(NewUser.ImageUpload.FileName);
+            Filename = Filename + extension;
+            Filename = Path.Combine(Server.MapPath("~/ProfileImage/"), Filename);
+            Guid activationCode = Guid.NewGuid();
+            NewUser.ActivationGuid = activationCode.ToString();
+
+            NewUser1.NewUser(NewUser,Filename);
+            string link = "<br/><a href = '" + string.Format("{0}://{1}/Home/Activation/{2}", Request.Url.Scheme, Request.Url.Authority, NewUser.ActivationGuid) + "'>Click here to activate your account.</a>";
+            NewUser.ActivationEmail(NewUser, link);
+                
+            return RedirectToAction("Register");
+                
+            
+        }
+
+        public ActionResult Activation()
+        {
+            ViewBag.ActivationMessage = "Invalid Activation code";
+            if (RouteData.Values["id"] != null)
+            {
+                string Activationcode = RouteData.Values["id"].ToString();
+                using (dbProjectManagementEntities db = new dbProjectManagementEntities())
+                {
+                    tblUser ToActivate = db.tblUsers.Where(m => m.GUID == Activationcode).SingleOrDefault();
+                    if (ToActivate != null)
+                    {
+                        ToActivate.UserStatus = true;
+                        db.SaveChanges();
+                        ViewBag.ActivationMessage = "Activated Successfully";
+
+                    }
+                }
+            }
+            return View();
+
+        }
+
+
+        
+
+    }
+
+
+}
---
+++ Documents/Visual Studio 2015/Projects/Project-Management/Project-Management/Models/UserRegistration.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Net;
+using System.IO;
+using System.Data.Entity;
+
+namespace Project_Management.Models
+{
+    [System.Runtime.InteropServices.Guid("E73A310F-97A7-44F9-8F50-6371B995C5BD")]
+    public class UserRegistration
+    {
+        [Required(ErrorMessage = "Enter Your Name")]
+        [Display(Name = "Enter Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Date of Birth")]
+        [Display(Name = "Enter DOB")]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Gender")]
+        [Display(Name = "Enter Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Upload your profile image")]
+        [Display(Name = "upload file")]
+        public string ProfileImage { get; set; }
+
+        
+
+        [Required(ErrorMessage = "Enter Your Email")]
+        [Display(Name = "Enter Email Address")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Password")]
+        [Display(Name = "Enter Password")]
+        [DataType(DataType.Password)]
+        //[RegularExpression("^ (?=.*?[A - Z])(?=.*?[a - z])(?=.*?[0 - 9])(?=.*?[#?!@$%^&*-]).{8,}$")]
+        public string UserPassword { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Bio")]
+        [Display(Name = "Enter Bio")]
+        public string UserBio { get; set; }
+
+        [Required(ErrorMessage = "Enter Your organisation")]
+        [Display(Name = "Enter organisation")]
+        public string UserOrganisation { get; set; }
+
+        // [Remote("IsUserNameAvailable","Home",ErrorMessage = "User Name already exist")]
+        [Required(ErrorMessage = "Enter Your Username")]
+        [Display(Name = "Enter Ussername")]
+        [StringLength(maximumLength: 8, MinimumLength = 5, ErrorMessage = "Username must be maximum 8 and minimum 5 characters long")]
+        public string UserUserName { get; set; }
+
+        public string ActivationGuid { get; set; }
+        
+
+        public HttpPostedFileBase ImageUpload { get; set; }
+       // public string UserStatus { get; set; }
+
+
+
+        public void NewUser(UserRegistration NewUser,string Filename)
+        {
+            
+
+
+            using (dbProjectManagementEntities db = new dbProjectManagementEntities())
+            {
+                tblUser CreateUser = new tblUser();
+                CreateUser.UserName = NewUser.UserName;
+                CreateUser.UserDOB = NewUser.DOB;
+                CreateUser.UserGender = NewUser.Gender;
+                CreateUser.UserProfileImage = NewUser.ProfileImage;
+                CreateUser.UserEmail = NewUser.UserEmail;
+                CreateUser.UserPassword = NewUser.UserPassword;
+                CreateUser.UserUserName = NewUser.UserName;
+                CreateUser.UserOrganisation = NewUser.UserOrganisation;
+                CreateUser.UserBio = NewUser.UserBio;
+                CreateUser.UserStatus = false;
+                CreateUser.GUID = NewUser.ActivationGuid;
+                string a = NewUser.ActivationGuid;
+
+
+                NewUser.ImageUpload.SaveAs(Filename);
+                string Filename1 = Path.GetFileNameWithoutExtension(NewUser.ImageUpload.FileName);
+                string extension = Path.GetExtension(NewUser.ImageUpload.FileName);
+                Filename1 = Filename1 + extension;
+
+                CreateUser.UserProfileImage = "~/ProfileImage/" + Filename1;
+
+                db.tblUsers.Add((CreateUser));
+                db.SaveChanges();
+
+
+            }
+
+            
+        }
+
+        public void ActivationEmail(UserRegistration NewUser,string part)
+        {
+            using (dbProjectManagementEntities db = new dbProjectManagementEntities())
+            {
+                tblUser CreatedUser = new tblUser();
+                using (MailMessage mm = new MailMessage("Aman.klmishra1715.am@gmail.com", NewUser.UserEmail))
+                {
+                    mm.Subject = "Account Activation";
+                    string body = "Hello" + NewUser.UserName + ",";
+                    body += "<br/><br/>please click the following link to activate your account";
+                    body += part;
+                    body += "<br/><br />Thanks";
+                    mm.Body = body;
+                    mm.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential("projectmanagementcommunity@gmail.com", "Akshay@123");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+
+
+                }
+            }
+        }
+    }
+}
